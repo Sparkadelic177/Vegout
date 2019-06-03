b5be3ab8707856b8d206851eb260f1f0ff282f9f
Sparkadelic177 <sparkyevangelista@gmail.com>
2019-06-03 09:48:21 -0400

    Updates

---
+++ precache-manifest.43df4d1c84a0e29e265297cc85a24dfa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "027905bcebbc7973af2461a6901f0459",
+    "url": "/Vegout/index.html"
+  },
+  {
+    "revision": "04b505510b8b2df57f52",
+    "url": "/Vegout/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/Vegout/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "04b505510b8b2df57f52",
+    "url": "/Vegout/static/js/main.05e85834.chunk.js"
+  },
+  {
+    "revision": "b7bdb28cb6fe6a067d5f",
+    "url": "/Vegout/static/js/runtime~main.56829d04.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Vegout/static/media/logo.5d5d9eef.svg"
+  }
+]);
